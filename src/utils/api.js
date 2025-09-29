b2ec566b226731e72c0d30d2833e4db27a90de88
yadhukrishnaofficial <yadhukrish605@gmail.com>
2025-09-29 16:20:03 +0530

    updated new url of render

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://alreem-7r91.onrender.com/",
+  baseURL: "https://al-reem-backend.onrender.com",
   withCredentials: true,
 });
 
